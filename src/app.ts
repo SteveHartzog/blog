68948a0ed8b17d8846abc039410333d515755118
Steve Hartzog <shartzog@gmail.com>
2017-05-06 01:13:27 -0400

    Use vanilla js to do animation. Closes #5

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,33 @@ export class App {
     this.bConf = this.blogConfig;
   }
 
+  smoothScrollTo(elementId, duration) {
+    let target = document.getElementById(elementId).offsetTop;
+    let offset = window.pageYOffset;
+    let delta  = target - window.pageYOffset; // Y-offset difference
+    duration = duration || 1000;              // default 1 sec animation
+    let start = Date.now();                   // get start time
+    let timer;
+
+    if( timer ) {
+      clearInterval(timer); // stop any running animation
+    }
+
+    function step() {
+      let y;
+      let factor = (Date.now() - start) / duration; // get interpolation factor
+      if( factor >= 1 ) {
+        clearInterval(timer); // stop animation
+        factor = 1;           // clip to max 1.0
+      }
+      y = factor * delta + offset;
+      window.scrollBy(0, y - window.pageYOffset);
+    }
+
+    timer = setInterval(step, 10);
+    return timer; // return the interval timer, so you can clear it elsewhere
+  }
+
   configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'Home';
     config.addPipelineStep('postcomplete', PostCompleteStep);
