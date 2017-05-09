80f538cbbc0d32581c8766e25d7cb3b231b2c3cf
vheissu <dwaynecharrington@gmail.com>
2017-05-09 18:40:17 +1000

    feat(interfaces): strongly type data in our app
    In TypeScript, interfaces are everything.

---
+++ src/interfaces.ts
@@ -0,0 +1,30 @@
+export type ContentType = 'post' | 'page';
+export type ContentStatus = 'published' | 'draft' | 'scheduled' | 'pending-review';
+
+export interface SiteConfigInterface {
+  siteTitle: string | null;
+  siteBlurb: string | null;
+  siteName: string;
+  siteDescription: string;
+  comments: {
+    enabled: boolean;
+  };
+}
+
+export interface ContentInterface {
+  title: string;
+  category: string;
+  excerpt: string;
+  content: string;
+  author: string;
+  views: number;
+  type: ContentType;
+  status: ContentStatus;
+  created_at: number;
+  updated_at: number;
+}
+
+export interface AuthorInterface {
+  avatar?: string;
+  name: string;
+}
