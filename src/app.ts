d52b3d3650b45e5b01e6ac1452b0a61b20e539df
vheissu <dwaynecharrington@gmail.com>
2017-05-09 13:02:13 +1000

    chore(app): cleanup routes and spacing of things mostly

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { Aurelia, autoinject } from 'aurelia-framework';
 import { Router, RouterConfiguration } from 'aurelia-router';
 import { PLATFORM } from 'aurelia-pal';
+
 import { Config } from './services/config';
 
 @autoinject
@@ -41,16 +42,23 @@ export class App {
 
   configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'Home';
+
     config.addPipelineStep('postcomplete', PostCompleteStep);
+
     config.options.pushState = true;
+    
     config.map([
-      { route: [''], name: 'home', moduleId: PLATFORM.moduleName('./pages/home'), nav: true, title: 'Home' },
-      { route: ['blog'], name: 'blog', moduleId: PLATFORM.moduleName('./pages/blog'), nav: false, title: 'Blog' },
-      { route: ['post/:url'], name: 'post', moduleId: PLATFORM.moduleName('./pages/post'), nav: false, title: 'Post' },
-      { route: ['about'], name: 'about', moduleId: PLATFORM.moduleName('./pages/about'), nav: true, title: 'About' },
-      // { route: ['portfolio'], name: 'portfolio', moduleId: PLATFORM.moduleName('./pages/portfolio'), nav: true, title: 'Portfolio' },
-      // { route: ['contact'], name: 'contact', moduleId: PLATFORM.moduleName('./pages/contact'), nav: true, title: 'Contact' },
-      { route: ['category/:cat'], name: 'category', moduleId: PLATFORM.moduleName('./pages/category'), nav: false, title: 'Category' }
+      { route: [''], name: 'home', moduleId: PLATFORM.moduleName('pages/home'), nav: true, title: 'Home' },
+
+      { route: ['blog'], name: 'blog', moduleId: PLATFORM.moduleName('pages/blog'), nav: false, title: 'Blog' },
+
+      { route: ['post/:url'], name: 'post', moduleId: PLATFORM.moduleName('pages/post'), nav: false, title: 'Post' },
+
+      { route: ['about'], name: 'about', moduleId: PLATFORM.moduleName('pages/about'), nav: true, title: 'About' },
+
+      { route: ['category/:cat'], name: 'category', moduleId: PLATFORM.moduleName('pages/category'), nav: false, title: 'Category' },
+      
+      { route: 'setup', name: 'setup', moduleId: PLATFORM.moduleName('setup/setup'), nav: true, title: 'Setup' },
     ]);
 
     this.router = router;
