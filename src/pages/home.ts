9c12423ae303c69e2d15b237332241fa7b3b40d5
vheissu <dwaynecharrington@gmail.com>
2017-05-09 16:38:34 +1000

    feat(app): pull content on homepage of post type by default

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -4,11 +4,15 @@ import { DataService } from '../services/dataService';
 @inject(DataService)
 export class Posts{
   posts: BlogPost[];
+  content: any;
 
   constructor (private ds) {
   }
   
   async activate(): Promise<void> {
+    this.content = await this.ds.getContent();
     this.posts = await this.ds.loadPosts();
+
+    console.log(this.content);
   }
 }
