22f0bcf6a2c63ea6b589d1790362fd6543a4be88
Steve Hartzog <SteveHartzog@users.noreply.github.com>
2017-05-22 11:10:56 -0400

    fix(post) closes #23

--- src/pages/post.ts
+++ src/pages/post.ts
@@ -3,27 +3,29 @@ import { DataService } from '../services/dataService';
 import { Markdown } from '../services/markdown';
 import { Config } from '../services/config';
 
-
 @inject(Config, DataService, Markdown)
 export class Post{
   post: BlogPost;
 
   constructor (private bConf: Config, private ds, private md: Markdown) {
+    // show titlebar when past the title
+    window.addEventListener('scroll', function() {
+      let titlebar = document.getElementById('titlebar');
+      let postBody = document.getElementById('postbody');
+      if (postBody) {
+        if (window.scrollY > (postBody['offsetTop'] - 100)) {
+          titlebar.style.display = 'block';
+        } else {
+          titlebar.style.display = 'none';
+        }
+      }
+    });
+    // Reset scroll height carried over from home
+    window.scrollTo(0, 0);
   }
   
   async activate(params): Promise<void> {
     this.post = await this.ds.getPostByUrl(params.url);
     this.post['fullUrl'] = encodeURIComponent(window.location.href.replace('/post/', '/#/post/'));
-
-    // show titlebar when past the title
-    window.addEventListener('scroll', function() {
-      let titlebar = document.getElementById('titlebar');
-      let postBody = document.getElementById('postbody');
-      if (window.scrollY > (postBody['offsetTop'] - 100)) {
-        titlebar.style.display = 'block';
-      } else {
-        titlebar.style.display = 'none';
-      }
-    });
   }
 }
