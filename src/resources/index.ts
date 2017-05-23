5b646f4f821b752c0b8106cb9fe569102a693e0e
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-05-23 21:44:16 +0100

    feat(comments): a comments component for displaying and authoring comments

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -2,6 +2,7 @@ import {FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
+    PLATFORM.moduleName('./elements/comments/comments'),
     PLATFORM.moduleName('./value-converters/date-format')
   ]);
 }
