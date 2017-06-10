72abf85250229f022a596bf8ec4e151339115bb0
carlos <carlos@moranjr.com>
2017-06-10 01:10:08 -0400

    blog-3 markdown and highlight js formatting.  Removes old styles

--- src/services/markdown.ts
+++ src/services/markdown.ts
@@ -3,21 +3,21 @@ import * as emoji from 'markdown-it-emoji';
 import * as twemoji from 'twemoji';
 import * as deflist from 'markdown-it-deflist';
 import * as hljs from 'highlight.js';
-window['hljs'] = hljs;
 
 export class Markdown {
   constructor() {
-    let md = new MarkdownIt({
+
+    const md = new MarkdownIt({
       html:         true,        // Enable HTML tags in source
       xhtmlOut:     true,        // Use '/' to close single tags (<br />).
                                   // This is only for full CommonMark compatibility.
-      breaks:       true,        // Convert '\n' in paragraphs into <br>
+      breaks:       false,        // Convert '\n' in paragraphs into <br>
       langPrefix:   'language-',  // CSS language prefix for fenced blocks. Can be
                                   // useful for external highlighters.
       linkify:      true,        // Autoconvert URL-like text to links
 
       // Enable some language-neutral replacement + quotes beautification
-      typographer:  false,
+      typographer:  true,
 
       // Double + single quotes replacement pairs, when typographer enabled,
       // and smartquotes on. Could be either a String or an Array.
@@ -29,10 +29,12 @@ export class Markdown {
       // Highlighter function. Should return escaped HTML,
       // or '' if the source string is not changed and should be escaped externaly.
       // If result starts with <pre... internal wrapper is skipped.
-      highlight: function (str, lang) { 
+      highlight: function (str, lang) {
         if (lang && hljs.getLanguage(lang)) {
           try {
-            return hljs.highlight(lang, str).value;
+            return '<pre class="hljs"><code>' +
+                    hljs.highlight(lang, str, true).value +
+                    '</code></pre>';
           } catch (__) {}
         }
         return ''; 
@@ -43,6 +45,6 @@ export class Markdown {
     md.renderer.rules['emoji'] = function(token, idx) {
       return twemoji.parse(token[idx].content);
     };
-    return md;
+  return md;
   }
 }
